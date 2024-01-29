cbb86a8aaf6dda49dd6c47eee7773893e07ecda4
Denis Prado Forigo <denisforigo@gmail.com>
2024-01-29 11:32:35 -0300

    api

--- apps/frontend/src/app/_api/fetchComments.ts
+++ apps/frontend/src/app/_api/fetchComments.ts
@@ -1,19 +1,23 @@
-import type { Comment, Post, User } from '../../payload/payload-types'
-import { COMMENTS_BY_DOC, COMMENTS_BY_USER } from '../_graphql/comments'
-import { GRAPHQL_API_URL } from './shared'
+import type {
+  Comment,
+  Post,
+  User,
+} from "../../../../admin/src/payload/payload-types";
+import { COMMENTS_BY_DOC, COMMENTS_BY_USER } from "../_graphql/comments";
+import { GRAPHQL_API_URL } from "./shared";
 
 export const fetchComments = async (args: {
-  user?: User['id']
-  doc?: Post['id']
+  user?: User["id"];
+  doc?: Post["id"];
 }): Promise<Comment[]> => {
-  const { user, doc } = args || {}
+  const { user, doc } = args || {};
 
   const docs: Comment[] = await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
-    cache: 'no-store',
+    cache: "no-store",
     body: JSON.stringify({
       query: user ? COMMENTS_BY_USER : COMMENTS_BY_DOC,
       variables: {
@@ -22,12 +26,13 @@ export const fetchComments = async (args: {
       },
     }),
   })
-    ?.then(res => res.json())
-    ?.then(res => {
-      if (res.errors) throw new Error(res?.errors?.[0]?.message ?? 'Error fetching docs')
+    ?.then((res) => res.json())
+    ?.then((res) => {
+      if (res.errors)
+        throw new Error(res?.errors?.[0]?.message ?? "Error fetching docs");
 
-      return res?.data?.Comments?.docs
-    })
+      return res?.data?.Comments?.docs;
+    });
 
-  return docs
-}
+  return docs;
+};
