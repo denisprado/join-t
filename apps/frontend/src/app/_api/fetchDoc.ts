f1ff1d39504d8b90fc62d6d6e25e6fceeab9caeb
Denis Prado Forigo <denisforigo@gmail.com>
2024-01-29 10:56:44 -0300

    api

--- apps/frontend/src/app/_api/fetchDoc.ts
+++ apps/frontend/src/app/_api/fetchDoc.ts
@@ -1,32 +1,35 @@
 import type { RequestCookie } from "next/dist/compiled/@edge-runtime/cookies";
 
 import type { Config } from "../../payload/payload-types";
-import { PAGES } from "../_graphql/pages";
-import { POSTS } from "../_graphql/posts";
-import { PROJECTS } from "../_graphql/projects";
+import { PAGE } from "../_graphql/pages";
+import { POST } from "../_graphql/posts";
+import { PROJECT } from "../_graphql/projects";
 import { GRAPHQL_API_URL } from "./shared";
 import { payloadToken } from "./token";
 
 const queryMap = {
   pages: {
-    query: PAGES,
+    query: PAGE,
     key: "Pages",
   },
   posts: {
-    query: POSTS,
+    query: POST,
     key: "Posts",
   },
   projects: {
-    query: PROJECTS,
+    query: PROJECT,
     key: "Projects",
   },
 };
 
-export const fetchDocs = async <T>(
-  collection: keyof Config["collections"],
-  draft?: boolean,
-  variables?: Record<string, unknown>
-): Promise<T[]> => {
+export const fetchDoc = async <T>(args: {
+  collection: keyof Config["collections"];
+  slug?: string;
+  id?: string;
+  draft?: boolean;
+}): Promise<T> => {
+  const { collection, slug, draft } = args || {};
+
   if (!queryMap[collection])
     throw new Error(`Collection ${collection} not found`);
 
@@ -37,26 +40,28 @@ export const fetchDocs = async <T>(
     token = cookies().get(payloadToken);
   }
 
-  const docs: T[] = await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
+  const doc: T = await fetch(`${GRAPHQL_API_URL}/api/graphql`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       ...(token?.value && draft ? { Authorization: `JWT ${token.value}` } : {}),
     },
     cache: "no-store",
-    next: { tags: [collection] },
+    next: { tags: [`${collection}_${slug}`] },
     body: JSON.stringify({
       query: queryMap[collection].query,
-      variables,
+      variables: {
+        slug,
+        draft,
+      },
     }),
   })
     ?.then((res) => res.json())
     ?.then((res) => {
       if (res.errors)
-        throw new Error(res?.errors?.[0]?.message ?? "Error fetching docs");
-
-      return res?.data?.[queryMap[collection].key]?.docs;
+        throw new Error(res?.errors?.[0]?.message ?? "Error fetching doc");
+      return res?.data?.[queryMap[collection].key]?.docs?.[0];
     });
 
-  return docs;
+  return doc;
 };
