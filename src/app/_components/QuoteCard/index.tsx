9abd533c36daee59f04d70644bfd3a41f0cff3dd
Denis Prado Forigo <denisforigo@gmail.com>
2024-02-05 09:25:07 -0300

    fix quote photos

--- src/app/_components/QuoteCard/index.tsx
+++ src/app/_components/QuoteCard/index.tsx
@@ -5,6 +5,7 @@ import ReactMarkdown from 'react-markdown'
 
 const QuoteCard = ({ quote, i, revert }: { quote: Quote, i: number, revert: boolean }) => {
 	const direction = revert ? 'flex-row-reverse' : 'flex-row'
+	const photo = "/" + quote.avatar
 	return (
 		<div className={`flex-col-reverse lg:flex-row bg-secondary shadow-xl gap-8 rounded-xl p-8 flex ${direction}`} id={`${quote.id}`} >
 			<div className="w-full">
@@ -14,7 +15,7 @@ const QuoteCard = ({ quote, i, revert }: { quote: Quote, i: number, revert: bool
 				</div>
 			</div>
 			<div className='flex flex-col gap-3 items-center min-w-64 justify-center bg-[url(/quote.png)] bg-center bg-no-repeat'>
-				<figure className='p-5'><Image className=' rounded-full' src={"/" + quote.avatar} alt="Author" width={112} height={112} /></figure>
+				<figure className='p-5'><Image className=' rounded-full' src={photo} alt={`Foto de ${quote.author}`} width={112} height={112} /></figure>
 				<p className='font-serif text-primary'>{quote.author}</p>
 			</div>
 		</div>
