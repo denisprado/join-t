45003af293d1c770e883c564d568c9635fcd550c
Denis Forigo <denisforigo@gmail.com>
2024-02-27 12:06:43 -0300

    customize admin panel with de right way

--- src/components/AdminApp.tsx
+++ src/components/AdminApp.tsx
@@ -14,43 +14,70 @@ import { PlansTypesList } from '@/app/_components/ReactAdmin/PlansTypes/List';
 import { authProvider } from '@/app/_supabase/authProvider';
 import { dataProvider } from '@/app/_supabase/dataProvider';
 import { ForgotPasswordPage, LoginPage, SetPasswordPage } from 'ra-supabase-ui-materialui';
-import { Admin, CustomRoutes, EditGuesser, ListGuesser, Resource, ShowGuesser } from 'react-admin';
+import { Admin, CustomRoutes, EditGuesser, ListGuesser, Resource, ShowGuesser, defaultTheme, defaultDarkTheme } from 'react-admin';
 import { Route } from 'react-router-dom';
 import { MyLayout } from './DevLayout';
 import { QuoteEdit } from '@/app/_components/ReactAdmin/Quote/Edit';
 import { QuoteCreate } from '@/app/_components/ReactAdmin/Quote/Create';
 import { QuoteList } from '@/app/_components/ReactAdmin/Quote/List';
 
-const MyAdmin = () => (
+const MyAdmin = () => {
+	// top: 80px !important;
+	// background-color: rgb(var(--yellow)) !important;
+	// color: rgb(var(--neutral)) !important;
+	const theme = {
+		...defaultDarkTheme,
+		components: {
+			...defaultDarkTheme.components,
+			MuiToolbar: {
+				styleOverrides: {
+					root: {
+						color: 'rgb(var(--neutral))'
+					}
+				}
+			},
+			RaAppBar: {
+				styleOverrides: {
+					root: {
+						backgroundColor: 'rgb(var(--yellow))',
+						top: '80px'
+					}
+				}
+			}
+		}
+	};
+	return (
 
-	<Admin
-		title={'JoinT'}
-		layout={MyLayout}
-		basename="/admin"
-		dataProvider={dataProvider}
-		authProvider={authProvider}
-		loginPage={LoginPage}
-	>
-		<CustomRoutes noLayout>
-			<Route
-				path={SetPasswordPage.path}
-				element={<SetPasswordPage />}
-			/>
-			<Route
-				path={ForgotPasswordPage.path}
-				element={<ForgotPasswordPage />}
-			/>
-		</CustomRoutes>
-		<Resource name="activity" list={ActivityList} edit={ActivityEdit} options={{ label: 'Atividades' }} show={ShowGuesser} create={ActivityCreate} />
-		<Resource name="activity_groups" list={ActivityGroupsList} edit={ActivityGroupsEdit} show={ShowGuesser} options={{ label: 'Grupo de Atividades' }} create={ActivityGroupsCreate} />
-		<Resource name="plans" list={PlansList} edit={PlansEdit} show={PlansCreate} options={{ label: 'Planos' }} create={PlansCreate} />
-		<Resource name="terms_groups" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} options={{ label: 'Grupos de Condições' }} create={EditGuesser} />
-		<Resource name="plan_types" list={PlansTypesList} edit={PlansTypesEdit} show={ShowGuesser} options={{ label: 'Tipos de Plano' }} create={EditGuesser} />
-		<Resource name="profile" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} options={{ label: 'Redes Sociais e Contatos' }} create={EditGuesser} />
-		<Resource name="quote" list={QuoteList} edit={QuoteEdit} show={ShowGuesser} options={{ label: 'Depoimentos de Alunos' }} create={QuoteCreate} />
+		<Admin
+			theme={theme}
+			title={'JoinT'}
+			layout={MyLayout}
+			basename="/admin"
+			dataProvider={dataProvider}
+			authProvider={authProvider}
+			loginPage={LoginPage}
+		>
+			<CustomRoutes noLayout>
+				<Route
+					path={SetPasswordPage.path}
+					element={<SetPasswordPage />}
+				/>
+				<Route
+					path={ForgotPasswordPage.path}
+					element={<ForgotPasswordPage />}
+				/>
+			</CustomRoutes>
+			<Resource name="activity" list={ActivityList} edit={ActivityEdit} options={{ label: 'Atividades' }} show={ShowGuesser} create={ActivityCreate} />
+			<Resource name="activity_groups" list={ActivityGroupsList} edit={ActivityGroupsEdit} show={ShowGuesser} options={{ label: 'Grupo de Atividades' }} create={ActivityGroupsCreate} />
+			<Resource name="plans" list={PlansList} edit={PlansEdit} show={PlansCreate} options={{ label: 'Planos' }} create={PlansCreate} />
+			<Resource name="terms_groups" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} options={{ label: 'Grupos de Condições' }} create={EditGuesser} />
+			<Resource name="plan_types" list={PlansTypesList} edit={PlansTypesEdit} show={ShowGuesser} options={{ label: 'Tipos de Plano' }} create={EditGuesser} />
+			<Resource name="profile" list={ListGuesser} edit={EditGuesser} show={ShowGuesser} options={{ label: 'Redes Sociais e Contatos' }} create={EditGuesser} />
+			<Resource name="quote" list={QuoteList} edit={QuoteEdit} show={ShowGuesser} options={{ label: 'Depoimentos de Alunos' }} create={QuoteCreate} />
 
-	</Admin>
+		</Admin>
 
-);
+	)
+};
 
 export default MyAdmin
